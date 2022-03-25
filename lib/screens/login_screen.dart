cc1842c19bafd2978daec7acbe3050255aefcca8
Prerak Gada <prerakgada07@gmail.com>
2022-03-25 19:03:25 +0530

    Firebase connected login, register, welcome screen created Tested on Android

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const String id = 'login_screen';
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('LoginScreen');
+  }
+}
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String id = 'welcome_screen';
+
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('WelcomeScreen');
+  }
+}
