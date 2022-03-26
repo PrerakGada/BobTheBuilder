abb426d2bef0e2887336d37767b83e6704ce9aeb
Prerak Gada <prerakgada07@gmail.com>
2022-03-27 05:11:03 +0530

    Contrator working

--- lib/screens/Project Owner/add_contractor.dart
+++ lib/screens/Project Owner/add_contractor.dart
@@ -23,7 +23,7 @@ class _ExitConfirmationDialogState extends State<ExitConfirmationDialog> {
   late String name;
   late String aadhar;
   late String email;
-  late String phone_no;
+  // late String phone_no;
   late String project_assigned;
 
   @override
@@ -38,159 +38,161 @@ class _ExitConfirmationDialogState extends State<ExitConfirmationDialog> {
   _buildChild(BuildContext context) => Container(
       width: 350,
       height: 533,
-      child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Padding(
+      child: SingleChildScrollView(
+        child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Center(
-                  child: Text("Contractor Details",
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(
+                    child: Text("Contractor Details",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text("Name",
                     style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18
+                        fontWeight: FontWeight.w500
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Text("Name",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500
-                  ),
-                ),
-                SizedBox(height: 10,),
-                TextField(
-                  onChanged: (value) {
-                    name = value;
-                  },
-                  // enableInteractiveSelection: false,
-                  decoration: const InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                      BorderSide(color: Colors.black, width: 1.0),
+                  SizedBox(height: 10,),
+                  TextField(
+                    onChanged: (value) {
+                      name = value;
+                    },
+                    // enableInteractiveSelection: false,
+                    decoration: const InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                        BorderSide(color: Colors.black, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                        BorderSide(color: Colors.black, width: 1.0),
+                      ),
+                      hintText: 'Enter your Name',
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                      BorderSide(color: Colors.black, width: 1.0),
-                    ),
-                    hintText: 'Enter your Name',
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text("AADHAR",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500
+                  SizedBox(
+                    height: 10,
                   ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                TextField(
-                  maxLength: 12,
-                  keyboardType: TextInputType.number,
-                  onChanged: (value) {
-                    aadhar = value;
-                  },
-                  // enableInteractiveSelection: false,
-                  decoration: const InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                      BorderSide(color: Colors.black, width: 1.0),
+                  Text("AADHAR",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                      BorderSide(color: Colors.black, width: 1.0),
-                    ),
-                    hintText: 'Enter your phone number',
                   ),
-                ),
-                SizedBox(height: 10,),
-                Text("Email ID",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500
+                  SizedBox(
+                    height: 15,
                   ),
-                ),
-                SizedBox(height: 10,),
-                TextField(
-                  onChanged: (value) {
-                    email = value;
-                  },
-                  // enableInteractiveSelection: false,
-                  decoration: const InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                      BorderSide(color: Colors.black, width: 1.0),
+                  TextField(
+                    maxLength: 12,
+                    keyboardType: TextInputType.number,
+                    onChanged: (value) {
+                      aadhar = value;
+                    },
+                    // enableInteractiveSelection: false,
+                    decoration: const InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                        BorderSide(color: Colors.black, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                        BorderSide(color: Colors.black, width: 1.0),
+                      ),
+                      hintText: 'Enter your phone number',
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                      BorderSide(color: Colors.black, width: 1.0),
-                    ),
-                    hintText: 'Enter Email',
                   ),
-                ),
-                SizedBox(height: 10,),
-                Text("Project Assigned",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500
-                  ),
-                ),
-                SizedBox(height: 10,),
-                TextField(
-                  onChanged: (value) {
-                    project_assigned = value;
-                  },
-                  // enableInteractiveSelection: false,
-                  decoration: const InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                      BorderSide(color: Colors.black, width: 1.0),
+                  SizedBox(height: 10,),
+                  Text("Email ID",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide:
-                      BorderSide(color: Colors.black, width: 1.0),
-                    ),
-                    hintText: 'Enter Project Assigned',
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Center(
-                    child: ElevatedButton(
-                        child: Text('Submit'),
-                        onPressed: () async {
-                          try {
-                            final newUser =
-                            await _auth.createUserWithEmailAndPassword(
-                                email: email, password: aadhar);
-                            _firestore.collection('users').doc(email).set({
-                              'name': name,
-                              'aadhar': aadhar,
-                              'phone': phone_no,
-                              'role': 'contractor',
-                            });
+                  SizedBox(height: 10,),
+                  TextField(
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    // enableInteractiveSelection: false,
+                    decoration: const InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                        BorderSide(color: Colors.black, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                        BorderSide(color: Colors.black, width: 1.0),
+                      ),
+                      hintText: 'Enter Email',
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Text("Project Assigned",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  TextField(
+                    onChanged: (value) {
+                      project_assigned = value;
+                    },
+                    // enableInteractiveSelection: false,
+                    decoration: const InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                        BorderSide(color: Colors.black, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                        BorderSide(color: Colors.black, width: 1.0),
+                      ),
+                      hintText: 'Enter Project Assigned',
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Center(
+                      child: ElevatedButton(
+                          child: Text('Submit'),
+                          onPressed: () async {
+                            try {
+                              final newUser =
+                              await _auth.createUserWithEmailAndPassword(
+                                  email: email, password: aadhar);
+                              _firestore.collection('users').doc(email).set({
+                                'name': name,
+                                'aadhar': aadhar,
+                                // 'phone': phone_no,
+                                'role': 'contractor',
+                              });
 
-                            Navigator.pop(context);
-                          } catch (e) {
-                            print(e);
-                          }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          // background color
-                            primary: Colors.red[900],
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 50,
-                              vertical: 15,
-                            ))),
+                              Navigator.pop(context);
+                            } catch (e) {
+                              print(e);
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            // background color
+                              primary: Colors.red[900],
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 50,
+                                vertical: 15,
+                              ))),
+                    ),
                   ),
-                ),
-              ],
-            ),
-          )
+                ],
+              ),
+            )
+        ),
       ));
 }
--- lib/screens/Project Owner/contractor.dart
+++ lib/screens/Project Owner/contractor.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:spike_codeshastra/screens/Project%20Owner/add_contractor.dart';
 
+final _firestore = FirebaseFirestore.instance;
+
 class Contractor extends StatefulWidget {
   static const String id = 'projects';
+
   const Contractor({Key? key}) : super(key: key);
 
   @override
@@ -16,7 +20,14 @@ class _ContractorState extends State<Contractor> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          leading: IconButton(icon: Icon(Icons.arrow_back,), onPressed: () { Navigator.pop(context); },),
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
           title: Text("Contractors"),
         ),
         body: Padding(
@@ -24,46 +35,92 @@ class _ContractorState extends State<Contractor> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Center(child: TextButton.icon(onPressed: () {
-                DialogContractor.exit(context);
-              }, icon: Icon(Icons.add_circle, size: 26,), label: Text("Add",
-                style: TextStyle(
-                    fontSize: 18
-                ),
-              ))),
+              Center(
+                  child: TextButton.icon(
+                      onPressed: () {
+                        DialogContractor.exit(context);
+                      },
+                      icon: Icon(
+                        Icons.add_circle,
+                        size: 26,
+                      ),
+                      label: Text(
+                        "Add",
+                        style: TextStyle(fontSize: 18),
+                      ))),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text("Existing Contractors",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20
-                  ),
+                child: Text(
+                  "Existing Contractors",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                 ),
               ),
-              Expanded(
-                child: ListView(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 60,
-                      child: Card(
-                        elevation: 10,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 8.0, right: 8.0,top: 8.0, bottom: 8.0,),
-                          child: Center(
-                            child: Text("Shubh Zatakia",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w400,
-                              fontSize: 18
-                            ),
-                            ),
-                          ),
+              StreamBuilder<QuerySnapshot>(
+                  stream: _firestore.collection('users').snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Center(
+                        child: CircularProgressIndicator(
+                          backgroundColor: Colors.lightBlueAccent,
                         ),
+                      );
+                    }
+                    final contractors = snapshot.data!.docs;
+                    List<ContractorCard> contractorCards = [];
+                    for (var contractor in contractors) {
+                      final role = contractor.get('role');
+                      if (role == "contractor") {
+                        final name = contractor.get('name');
+
+                        final contractorCard = ContractorCard(
+                          name: name,
+                        );
+
+                        contractorCards.add(contractorCard);
+                      }
+                    }
+
+                    return SizedBox(
+                      height: 600,
+                      child: ListView(
+                        children: contractorCards,
                       ),
-                    ),
-                  ],
-                ),
-              ),
+                    );
+                  }),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ContractorCard extends StatelessWidget {
+  const ContractorCard({
+    Key? key,
+    required this.name,
+  }) : super(key: key);
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      child: Card(
+        elevation: 10,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 8.0,
+            right: 8.0,
+            top: 8.0,
+            bottom: 8.0,
+          ),
+          child: Center(
+            child: Text(
+              name,
+              style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),
+            ),
           ),
         ),
       ),
